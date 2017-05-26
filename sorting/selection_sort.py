44dc5435430d5b23cf957951807680bb97b912f9
yulianghua <992049896@qq.com>
2017-05-26 14:31:53 +0800

    add sorting/selectionsort.py file

---
+++ sorting/selection_sort.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+
+author:(Fisher@992049896@qq.com)
+~~~~~~~~~~~~~~~~~~~
+选择排序介绍:
+
+
+"""
+
+class SelectionSort(object):
+    def sort(data):
+        if len(data) == 0:
+            return 
+        if len(data) < 2:
+            return data
+
+        length = len(data)
+        for i in xrange(1, length):
+            pass
